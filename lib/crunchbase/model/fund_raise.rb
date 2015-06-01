60c90c060941a09c6de798bf4f1c485f08d5db2f
Encore Shao <encore@ekohe.com>
2015-06-01 17:03:03 +0800

    BUG - Fix the organization funds bugs, create new model FundRaise!

---
+++ lib/crunchbase/model/fund_raise.rb
@@ -0,0 +1,25 @@
+# encoding utf-8
+
+module Crunchbase::Model
+  class FundRaise < Crunchbase::Model::Entity
+
+    RESOURCE_LIST = 'funds'
+
+    attr_reader :name, :path, :created_at, :updated_at
+    
+    def initialize(json)
+      property_keys.each { |v| 
+        instance_variable_set("@#{v}", json[v] || nil) 
+      } 
+
+      %w[created_at updated_at].each { |v| 
+        instance_variable_set("@#{v}", Time.at(json[v])) 
+      } 
+    end
+
+    def property_keys
+      %w[ name path created_at updated_at ]
+    end
+
+  end
+end
