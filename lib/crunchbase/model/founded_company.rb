4f31b451e58b2523274af038d39cbd1a5f724223
Encore Shao <encore@ekohe.com>
2015-11-03 16:31:57 +0800

    MISC - Update get person's founded_companies API interface.

--- lib/crunchbase/model/founded_company.rb
+++ lib/crunchbase/model/founded_company.rb
@@ -1,27 +1,30 @@
 # encoding: utf-8
 
 module Crunchbase::Model
-  class FoundedCompany < Crunchbase::Model::Entity
+  class FoundedCompany < Crunchbase::Model::Organization
 
     RESOURCE_LIST = 'founded_companies'
 
-    attr_reader :type, :name, :path, :created_at, :updated_at
+    #
+    # 2015-11-03 Removed
+    #
+    # attr_reader :type, :name, :path, :created_at, :updated_at
 
-    def initialize(json)
-      property_keys.each { |v|
-        instance_variable_set("@#{v}", json[v] || nil)
-      }
+    # def initialize(json)
+    #   property_keys.each { |v|
+    #     instance_variable_set("@#{v}", json[v] || nil)
+    #   }
 
-      %w[created_at updated_at].each { |v|
-        instance_variable_set("@#{v}", Time.at(json[v]))
-      }
-    end
+    #   %w[created_at updated_at].each { |v|
+    #     instance_variable_set("@#{v}", Time.at(json[v]))
+    #   }
+    # end
 
-    def property_keys
-      %w[
-        type name path created_at updated_at
-      ]
-    end
+    # def property_keys
+    #   %w[
+    #     type name path created_at updated_at
+    #   ]
+    # end
 
   end
 end
